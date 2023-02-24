a1f3d1d1170fde9f7027c6b362efbde1a28e4565
Julian Bayer <jdbayer09@hotmail.com>
2023-02-23 21:58:00 -0500

    Configuracion inicial APP

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'co.com.jdbsoft.restaurantero.app',
   appName: 'Restaurantero',
   webDir: 'www',
   bundledWebRuntime: false
